8d65c4919693c80682fca2fed67f771256eb69cd
Imingen <marius.steller@gmail.com>
2016-11-02 16:15:35 +0100

    Add project files.

---
+++ Strength.App/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Strength.App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Strength.App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
