ef02913c25982e82944d99a7ad5b8e87ab238cd5
Imingen <marius.steller@gmail.com>
2016-11-02 17:01:36 +0100

    Added Models (portable)

---
+++ Strength.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strength.Models
+{
+   public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool Compound { get; set; }
+        public bool Isolation { get; set; }
+
+        public virtual ICollection<Excercise> Excersices { get; set; }
+    }
+}
---
+++ Strength.Models/Excercise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Strength.Models
+{
+    public class Excercise
+    {
+        public int ExerciseId { get; set; }
+        public string ExerciseName { get; set; }
+        public string ExcersiceDescription { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
