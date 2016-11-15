0cebff01d851252c3d1f31d14e619c13a1ca8f91
Imingen <marius.steller@gmail.com>
2016-11-15 15:07:33 +0100

    RIP!!!!????!?!??!! changed to community from enterprise, lets see if it will continue to work. First commit after changing

---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO dbo.Excercises(ExerciseId, ExerciseName, ExcersiceDescription, Image, Category)
+VALUES
+(1, 'Bench Press', 'Go full ham mah nigga', 'C:\Users\Marius\Pictures\Arnold.jpg', mnlkj);
